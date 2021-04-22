791e87aeb03196dee6f72064b45524f3914b560d
igoreta <ivoronline@gmail.com>
2021-04-23 00:55:29 +0200

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_security_automatic_authorizationheader/SpringbootSecurityAutomaticAuthorizationheaderApplication.java
@@ -0,0 +1,15 @@
+package com.ivoronline.springboot_security_automatic_authorizationheader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@SpringBootApplication
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SpringbootSecurityAutomaticAuthorizationheaderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootSecurityAutomaticAuthorizationheaderApplication.class, args);
+  }
+
+}
