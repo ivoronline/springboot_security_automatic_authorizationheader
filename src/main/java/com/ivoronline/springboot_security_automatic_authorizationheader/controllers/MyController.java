12e3bc2dcaea387cb63ec08ce6cddf9703c4936a
ivoronline <ivoronline@gmail.com>
2022-01-22 11:27:30 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_security_automatic_authorizationheader/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_security_automatic_authorizationheader/controllers/MyController.java
@@ -3,16 +3,18 @@ package com.ivoronline.springboot_security_automatic_authorizationheader.control
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @RestController
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class MyController {
 
   //==============================================================================
-  // HELLO (Secured Resource)
+  // HELLO
   //==============================================================================
   @Secured("ROLE_USER")
-  @RequestMapping("/Hello")
-  public String hello() {
+  @RequestMapping("Hello")
+   String hello() {
     return "Hello from Controller";
   }
 
